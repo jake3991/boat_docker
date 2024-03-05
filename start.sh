c59f8b3a37be4422d0ee9c15eac8f5d6615d1283
jake3991 <john.mcconnell.1874@gmail.com>
2024-03-05 16:02:12 -0500

    adding start script

---
+++ start.sh
@@ -0,0 +1,12 @@
+# you will find the usb device in the /sys/bus/usb folder
+# sidescan usb config
+sudo bash -c "echo -n 1-3.1:1.0 >/sys/bus/usb/drivers/ftdi_sio/unbind"
+sudo bash -c "echo -n 1-3.1:1.1 >/sys/bus/usb/drivers/ftdi_sio/unbind"
+
+# give permissions to the usb socket
+sudo chown root:boat /dev/bus/usb/001/*
+
+# start the docker image
+sudo docker run --rm -it --net=host --ipc=host --pid=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$DISPLAY" --privileged -v /dev/bus/usb:/dev/bus/usb wrc/boat:humble bash
+
+

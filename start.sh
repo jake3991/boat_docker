34439112f33957193f507cdfa3c1b0fdd6edf602
jake3991 <john.mcconnell.1874@gmail.com>
2024-03-06 16:17:04 -0500

    Updating start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ sudo bash -c "echo -n 1-3.1:1.1 >/sys/bus/usb/drivers/ftdi_sio/unbind"
 sudo chown root:boat /dev/bus/usb/001/*
 
 # start the docker image
-sudo docker run --rm -it --net=host --ipc=host --pid=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$DISPLAY" --privileged -v /dev/bus/usb:/dev/bus/usb wrc/boat:humble bash
+sudo docker run --rm -it --net=host --ipc=host --pid=host -v /media/boat/external_dr/jetson_data:/jetson_data -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$DISPLAY" --privileged -v /dev/serial:/dev/serial:ro -v /dev/bus/usb:/dev/bus/usb wrc/boat:humble bash
 
 
